4496357eb1f65cad2e00d93a575070ce096fc443
Luis Paulo França <franca.luiz.paulo.777@gmail.com>
2023-04-11 12:54:15 -0300

    Controle de Produtos: Aula 02 - FrontEnd

---
+++ script.js
@@ -0,0 +1,15 @@
+class Produto{
+    constructor(){
+        this.id = 0
+        this.nomeProduto = ''
+        this.valor = 0.0
+    }
+    adicionar(){
+        alert("Adicionar um novo produto")
+    }
+    excluir(){
+        alert("Produto excluído")
+    }    
+}
+
+let produto = new Produto
