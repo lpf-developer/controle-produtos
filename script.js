affdca9b8af3b0a498a4e35cb5d90146d257f0ff
Luis Paulo França <franca.luiz.paulo.777@gmail.com>
2023-04-11 15:25:20 -0300

    Controle de Produtos: Aula 03 - Validação e Flash Messages

--- script.js
+++ script.js
@@ -1,15 +1,73 @@
 class Produto{
+
     constructor(){
-        this.id = 0
-        this.nomeProduto = ''
-        this.valor = 0.0
+        this.id = 1
+        this.arrayProdutos = []
+        this.msg = ''
     }
+
+    lerDados(){
+
+        let produto = {}
+        
+        produto.id = this.id
+        produto.nomeProduto = document.getElementById('produto').value
+        produto.valor = document.getElementById('valor').value
+        
+        return produto
+    }
+
+    validaCampos(produto){
+
+        //let validaNumero = /^[-]?\d*\.?\d+$/
+        
+        if(produto.valor == ''){
+            this.msg += "- Informe o valor do produto<br>"
+            document.getElementById("valor").focus()
+        }
+
+        // if (validaNumero.test(produto.valor)){
+        //     msg += "- Valor inválido do produto"
+        //     document.getElementById("valor").focus()
+        // }
+
+        if(produto.nomeProduto == '') {
+            this.msg += "- Informe o nome do produto<br>"
+            document.getElementById("produto").focus()
+        }
+        
+        if(this.msg != ''){
+            this.showFlashMessage(this.msg,3000)
+            this.msg = ''
+            return false
+        }
+
+        return true
+    }
+
+    salvar(){
+        let produto = this.lerDados()
+        if(this.validaCampos(produto)){
+            alert('salvar')
+        }
+    }
+
     adicionar(){
-        alert("Adicionar um novo produto")
+        
     }
+
     excluir(){
         alert("Produto excluído")
     }    
+
+    showFlashMessage(message, timeout) {
+        let flashMessage = document.getElementById('flash-message');
+        flashMessage.innerHTML = message;
+        flashMessage.style.display = 'block';
+        setTimeout(function () {
+            flashMessage.style.display = 'none';
+        }, timeout);
+    }
 }
 
 let produto = new Produto
